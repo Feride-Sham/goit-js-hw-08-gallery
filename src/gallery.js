fa6aa2a31b5911ba4c24659ec2ccaf610173a3d0
Feride Shamsutdinova <ferika.sham@gmail.com>
2020-11-19 22:30:42 +0300

    Adds swipes

--- src/gallery.js
+++ src/gallery.js
@@ -1,20 +1,21 @@
-import imgArray from "./gallery-items.js";
+import imgArray from './gallery-items.js';
 
-const imgLightbx = document.querySelector(".lightbox__image");
-const galleryList = document.querySelector(".js-gallery");
+const imgLightbx = document.querySelector('.lightbox__image');
+const galleryList = document.querySelector('.js-gallery');
 
-const lightBox = document.querySelector(".js-lightbox");
-const btnClose = document.querySelector(".lightbox__button");
+const lightBox = document.querySelector('.js-lightbox');
+const btnClose = document.querySelector('.lightbox__button');
 const imgCard = createImgCard(imgArray);
+let currentImgIndex;
 
-galleryList.insertAdjacentHTML("beforeend", imgCard);
-galleryList.addEventListener("click", onGalleryClick);
-btnClose.addEventListener("click", btnCloseClick);
+galleryList.insertAdjacentHTML('beforeend', imgCard);
+galleryList.addEventListener('click', onGalleryClick);
+btnClose.addEventListener('click', btnCloseClick);
 
 // функция создания разметки карточки в галерее
 function createImgCard(imgArray) {
   return imgArray
-    .map(({ preview, original, description }) => {
+    .map(({ preview, original, description }, index) => {
       return `<li class="gallery__item">
   <a
     class="gallery__link"
@@ -24,29 +25,27 @@ function createImgCard(imgArray) {
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
+      data-index="${index}"
       alt="${description}"
     />
   </a>
 </li>`;
     })
-    .join("");
+    .join('');
 }
 
-// call-back функция для клика по картинке
+// call-back функция для клика по картинке (открытие модального окна)
 function onGalleryClick(ev) {
   ev.preventDefault();
-
   const imgRef = ev.target;
-  // console.log(imgRef);
-  // const parentImgRef = imgRef.closest(".gallery__item");
-  // console.log(galleryList.children);
-  // console.log(parentImgRef);
+  const originalImgRef = imgRef.dataset.source;
 
-  if (imgRef.nodeName !== "IMG") {
+  currentImgIndex = Number(imgRef.getAttribute('data-index'));
+
+  if (imgRef.nodeName !== 'IMG') {
     return;
   }
 
-  const originalImgRef = imgRef.dataset.source;
   setLargeImgRef(originalImgRef);
   openModal(ev);
 }
@@ -58,14 +57,15 @@ function setLargeImgRef(source) {
 
 //функция открытия модалки
 function openModal(ev) {
-  lightBox.classList.add("is-open");
-  window.addEventListener("keydown", onEscDown);
-  // imgLightbx.setAttribute("data-index", value);
+  lightBox.classList.add('is-open');
+  window.addEventListener('keydown', onEscDown);
+  window.addEventListener('keydown', onRightDown);
+  window.addEventListener('keydown', onLeftDown);
 }
 
 // call-back функция для клика по кнопке закрытия модалки
 function btnCloseClick(ev) {
-  if (ev.target.nodeName !== "BUTTON") {
+  if (ev.target.nodeName !== 'BUTTON') {
     return;
   }
   closeModal(ev);
@@ -74,19 +74,21 @@ function btnCloseClick(ev) {
 // функция закрытия модального окна
 // +очистка src
 function closeModal(ev) {
-  lightBox.classList.remove("is-open");
+  lightBox.classList.remove('is-open');
 
-  // удаление слушателя на ESC(доп задание):
-  window.removeEventListener("keydown", onEscDown);
+  // удаление слушателей на ESC, влево, вправо(доп задание):
+  window.removeEventListener('keydown', onEscDown);
+  window.removeEventListener('keydown', onRightDown);
+  window.removeEventListener('keydown', onLeftDown);
 
   // очистка src, чтобы не мелькало большое изображение:
-  imgLightbx.src = "";
+  imgLightbx.src = '';
 }
 
 //******дополнительное задание********
-const overlayRef = document.querySelector(".lightbox__overlay");
+const overlayRef = document.querySelector('.lightbox__overlay');
 
-overlayRef.addEventListener("click", overlayRefClick);
+overlayRef.addEventListener('click', overlayRefClick);
 
 // call-back функция для клика по области модалки (закрытие)
 function overlayRefClick(ev) {
@@ -97,7 +99,31 @@ function overlayRefClick(ev) {
 
 // call-back функция для Esc (закрытие)
 function onEscDown(ev) {
-  if (ev.code === "Escape") {
+  if (ev.code === 'Escape') {
     closeModal(ev);
+  }
+}
+
+// call-back функция для кнопки Вправо (пролистывание)
+function onRightDown(ev) {
+  if (ev.code === 'ArrowRight') {
+    currentImgIndex += 1;
+    if (currentImgIndex === imgArray.length) {
+      currentImgIndex = 0;
+    }
+    imgLightbx.src = '';
+    imgLightbx.src = imgArray[currentImgIndex].original;
+  }
+}
+
+// call-back функция для кнопки Влево (пролистывание)
+function onLeftDown(ev) {
+  if (ev.code === 'ArrowLeft') {
+    currentImgIndex -= 1;
+    if (currentImgIndex === -1) {
+      currentImgIndex = imgArray.length - 1;
+    }
+    imgLightbx.src = '';
+    imgLightbx.src = imgArray[currentImgIndex].original;
   }
 }
